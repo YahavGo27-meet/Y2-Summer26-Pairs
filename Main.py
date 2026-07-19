ec35af2a458da7cf8421d662d87b067d023419a2
Rahaf2027-meet <rahaf24kh@gmail.com>
2026-07-19 18:26:51 +0300

    dd

--- Main.py
+++ Main.py
@@ -9,19 +9,33 @@ load_dotenv()
 client = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
 
 mode=""
-
 def main():
-    mode = input('''Do you need help with CS or Entrepreneurship? 
-    1-CS 
-    2-Entrepreneurship''')
-    if mode == "2":
-        run_chat2()
-        print()
-    elif mode == "1":
-        run_chat1()  
+        mode = input('''Do you need help with CS or Entrepreneurship? 
+        1-CS 
+        2-Entrepreneurship''')
 
+        while mode != "1" and mode != "2" and mode != "exit":
+            print("Invalid input. Please enter 1 for CS or 2 for Entrepreneurship.")
+            mode = input('''Do you need help with CS or Entrepreneurship? 
+            1-CS 
+            2-Entrepreneurship''')
+            
+            if mode == "2":
+                run_chat2()
+            elif mode == "1":
+                run_chat1() 
+
+                
+        # else:
+        #     while mode != "1" and mode != "2":
+        #         print("Invalid input. Please enter 1 for CS or 2 for Entrepreneurship.")
+        #         mode = input('''Do you need help with CS or Entrepreneurship?''' )
+        #         if 
+        #     #if mode == "exit":
+        #        # break
 
 if __name__ == "__main__":
     main()
+
 
     
